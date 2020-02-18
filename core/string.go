8d417d82a173d725bfa49d835a63654c68f9c43f
jianxinliu <1159322990@qq.com>
2020-02-18 20:50:12 +0800

    unnecessary to panic

--- core/string.go
+++ core/string.go
@@ -7,13 +7,13 @@ import (
 
 // Set return ture while set a string k-v pair
 // panic raise if k is a empty string
-func Set(k, v string) bool {
+func Set(k, v string) (bool,error) {
 	if len(k) == 0 {
-		panic("key cloud not be empty")
-		// return false, errors.New("key cloud not be empty!")
+		// panic("key cloud not be empty")
+		return false, errors.New("key cloud not be empty!")
 	}
 	Pool.strs[k] = v
-	return true
+	return true,nil
 }
 
 // Set k-v string with expire time, in seconds
